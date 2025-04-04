cf4fa574ed8301b28c98493b8ded4a80575855c9
Asterix265 <martin.bahamonde@ug.uchile.cl>
2025-04-04 13:05:02 -0300

    FEAT: added case 2 in lab_solutions, and the answer to c in README

--- Lab1/README.md
+++ Lab1/README.md
@@ -24,3 +24,7 @@ Para este caso se uso la respuesta de "12345678", dicha respuesta cifrada, la pa
 ### b.- 
 Al enviar al servidor B la respuesta de A obtenemos el mensaje inicial cifrado, lo que nos dice que el servidor B reconoce la clave y puede des encriptar, esto nos dice que podríamos intentar enviar mensajes al servidor B de los bits modificados (cifrado modificado) para ver sus respuestas
 
+### c.- 
+Para conocer el largo del cifrador se puede enviar un mensaje de largo L_m al cifrador, este enviará una cadena de largo L_c, luego, hay que iterar sumando un caracter de largo al mensaje (L_m) hasta conseguir que el largo de L_c cambie.
+
+Una vez logrado esto, hay que tener en consideración que el encriptador puede añadir una concatenacion al mensaje antes de cifrar, por ende, una vez haya cambiado, podemos saber que estamos con un bloque de solo padding, ahora, hay que ir sumando nuevamente caracteres a L_m hasta que L_c cambie, el número de caracteres sumados en esta iteración final, es el tamaño del bloque.
--- Lab1/src/descryptor.py
+++ Lab1/src/descryptor.py
@@ -5,17 +5,17 @@ DESCRYPTOR_ADDR = ("cc5327.hackerlab.cl", 5313)
 sock_input_encryptor, sock_output_encryptor= utils.create_socket(ENCRYPTOR_ADDR)
 
 sock_input_descryptor, sock_output_descryptor = utils.create_socket(DESCRYPTOR_ADDR)
-
-try:
-    response = input("send a message: ")
-    print("[Client] \"{}\"".format(response))
-    encrypt = utils.send_message(sock_input_encryptor, sock_output_encryptor, response)
-    print("[Server:5312] \"{}\"".format(encrypt))
-    print("Changing to descryptor...")
-    descrypt = utils.send_message(sock_input_descryptor,sock_output_descryptor , encrypt)
-    print("[Server:5313] \"{}\"".format(descrypt))
-    print("Closing...")
-except Exception as e:
-    print(e)
-    print("Closing...")
-    input.close()
+def dec():
+    try:
+        response = input("send a message: ")
+        print("[Client] \"{}\"".format(response))
+        encrypt = utils.send_message(sock_input_encryptor, sock_output_encryptor, response)
+        print("[Server:5312] \"{}\"".format(encrypt))
+        print("Changing to descryptor...")
+        descrypt = utils.send_message(sock_input_descryptor,sock_output_descryptor , encrypt)
+        print("[Server:5313] \"{}\"".format(descrypt))
+        print("Closing...")
+    except Exception as e:
+        print(e)
+        print("Closing...")
+        input.close()
